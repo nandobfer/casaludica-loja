2bd5bf99d3eea4310ba9b193bbd445a35dc8db17
tainarasn <tainarasn.sba@gmail.com>
2023-05-22 11:54:16 -0300

    feat: update product container

--- src/pages/Checkout/Product.tsx
+++ src/pages/Checkout/Product.tsx
@@ -1,21 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
+import { Product as ProductType } from "../../definitions/products"
 import { ReactComponent as CloseIcon } from "../../images/x.svg"
 
-interface ProductProps {}
+interface ProductProps {
+    product: ProductType
+}
 
-export const Product: React.FC<ProductProps> = ({}) => {
+export const Product: React.FC<ProductProps> = ({ product }) => {
+    const [productQuantity, setProductQuantity] = useState(product.quantity)
     return (
         <div className="product-container">
-            <img src="/product-image.png" alt="product" className="img-product" />
+            <img src={product.cover} alt="product" className="img-product" />
             <div className="right-container">
                 <p className="name-product">
-                    Produto: <span>Lorem Ipsum Dolor</span>
+                    Produto: <span>{product.name}</span>
                 </p>
                 <p className="quantity">
-                    Quantidade: <input type="number" className="number" />
+                    Quantidade:{" "}
+                    <input
+                        type="number"
+                        className="number"
+                        value={product.quantity}
+                        onChange={(event) => setProductQuantity(Number(event.target.value))}
+                    />
                 </p>
                 <p className="price-product">
-                    Preço: <span className="price">R$29,90</span>
+                    Preço: <span className="price">R${product.price}</span>
                 </p>
             </div>
             <CloseIcon className="close" />
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Background } from "../../components/Background"
 import { Header } from "../../components/Header"
 import { SearchField } from "../../components/SearchField"
+import { useCart } from "../../hooks/useCart"
 import { Collections } from "../Home/Collections"
 import { Product } from "./Product"
 import "./style.scss"
@@ -9,6 +10,8 @@ import "./style.scss"
 interface CheckoutProps {}
 
 export const Checkout: React.FC<CheckoutProps> = ({}) => {
+    const cart = useCart()
+
     return (
         <div className="Checkout-Component">
             <Background />
@@ -19,7 +22,9 @@ export const Checkout: React.FC<CheckoutProps> = ({}) => {
 
             <p className="finish">Finalizar Pedido</p>
             <div className="order">
-                <Product />
+                {cart.products.map((product) => (
+                    <Product key={product.id} product={product} />
+                ))}
             </div>
         </div>
     )
